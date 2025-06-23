f68c56baeb6ec0464b6f04f62d3dc42563419579
Kashyap1240 <shivamskashyap@gmail.com>
2025-06-23 08:48:11 +0530

    without static variable

--- CruxLive/src/lecture_18/Count_Subsequence.java
+++ CruxLive/src/lecture_18/Count_Subsequence.java
@@ -4,20 +4,17 @@ public class Count_Subsequence {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String n = sc.nextLine();
-        Print(n, "");
-        System.out.println("\n"+ count);
+        System.out.println("\n" + Print(n, ""));
     }
 
-    static int count = 0;
-
-    public static void Print(String ques, String ans){
+    public static int Print(String ques, String ans){
         if(ques.length() == 0){
             System.out.print(ans+ " ");
-            count++;
-            return;
+            return 1;
         }
         char ch = ques.charAt(0);
-        Print(ques.substring(1), ans);
-        Print(ques.substring(1),ans + ch );
+        int a = Print(ques.substring(1), ans);
+        int b = Print(ques.substring(1),ans + ch );
+        return a + b;
     }
 }
