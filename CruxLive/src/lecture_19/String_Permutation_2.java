7f1f70382d2b71b64faeb46d3245ec8eb8e1e6a7
Kashyap1240 <shivamskashyap@gmail.com>
2025-06-24 14:47:49 +0530

    for duplicates char

--- CruxLive/src/lecture_19/String_Permutation_2.java
+++ CruxLive/src/lecture_19/String_Permutation_2.java
@@ -2,6 +2,33 @@ package CruxLive.src.lecture_19;
 
 public class String_Permutation_2 {
     public static void main(String[] args) {
+        String ques = "abca";
+        Print(ques, "");
+    }
 
+    public static void Print(String ques, String ans) {
+        if (ques.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+        for (int i = 0; i < ques.length(); i++) {
+            char ch = ques.charAt(i);
+            if (Is_present(ques, ch, i + 1) == false) {
+                String s1 = ques.substring(0, i);
+                String s2 = ques.substring(i + 1);
+                Print(s1 + s2, ans + ch);
+            }
+        }
+
+    }
+
+    public static boolean Is_present(String str, char ch, int i) {
+
+        for (int j = i; j < str.length(); j++) {
+            if (str.charAt(j) == ch) {
+                return true;
+            }
+        }
+        return false;
     }
 }
