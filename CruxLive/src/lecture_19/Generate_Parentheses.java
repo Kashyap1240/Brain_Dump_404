5f78015be6f9176999f9612638345a273f158eb8
Kashyap1240 <shivamskashyap@gmail.com>
2025-06-24 10:19:46 +0530

    for leetcode

--- CruxLive/src/lecture_19/Generate_Parentheses.java
+++ CruxLive/src/lecture_19/Generate_Parentheses.java
@@ -1,25 +1,25 @@
 package CruxLive.src.lecture_19;
 import java.lang.reflect.Parameter;
-import java.util.Scanner;
+import java.util.*;
 public class Generate_Parentheses {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        Parentheses(n,0,0,"");
+        List<String> ll = new ArrayList<>();
+        Parentheses(n,0,0,"", ll);
     }
 
-    public static void Parentheses(int n, int open, int close, String ans){
+    public static void Parentheses(int n, int open, int close, String ans, List<String> ll){
         if(open == n && close == n){
-            System.out.println(ans);
+            // System.out.println(ans);
+            ll.add(ans);
             return;
         }
-
         if(open > n || close > open){
             return;
         }
 
-
-        Parentheses(n, open + 1, close, ans+ "(" );
-        Parentheses(n, open, close + 1, ans+ ")" );
+        Parentheses(n, open + 1, close, ans+ "(", ll);
+        Parentheses(n, open, close + 1, ans+ ")" , ll);
     }
 }
